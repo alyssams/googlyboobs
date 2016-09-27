0a934e13bead419975d15e1f5c341b3cb89f47a4
Alyssa M. Saucedo <asilla7@gmail.com>
2016-09-26 17:11:34 -0700

    updated
    new layout, added music, added fullscreen

--- js/main.js
+++ js/main.js
@@ -1,42 +1,132 @@
+jQuery(document).ready(function($){
+	var slidesWrapper = $('.cd-hero-slider');
 
+	//check if a .cd-hero-slider exists in the DOM 
+	if ( slidesWrapper.length > 0 ) {
+		var primaryNav = $('.cd-primary-nav'),
+			sliderNav = $('.cd-slider-nav'),
+			navigationMarker = $('.cd-marker'),
+			slidesNumber = slidesWrapper.children('li').length,
+			visibleSlidePosition = 0,
+			autoPlayId,
+			autoPlayDelay =5000;
 
-// Mouseover/ Click sound effect- by JavaScript Kit (www.javascriptkit.com)
-// Visit JavaScript Kit at http://www.javascriptkit.com/ for full source code
+		//upload videos (if not on mobile devices)
+		uploadVideo(slidesWrapper);
 
-//** Usage: Instantiate script by calling: var uniquevar=createsoundbite("soundfile1", "fallbackfile2", "fallebacksound3", etc)
-//** Call: uniquevar.playclip() to play sound
+		//autoplay slider
+		setAutoplay(slidesWrapper, slidesNumber, autoPlayDelay);
 
-var html5_audiotypes={ //define list of audio file extensions and their associated audio types. Add to it if your specified audio file isn't on this list:
-	"mp3": "audio/mpeg",
-	"mp4": "audio/mp4",
-	"ogg": "audio/ogg",
-	"wav": "audio/wav"
-}
+		//on mobile - open/close primary navigation clicking/tapping the menu icon
+		primaryNav.on('click', function(event){
+			if($(event.target).is('.cd-primary-nav')) $(this).children('ul').toggleClass('is-visible');
+		});
+		
+		//change visible slide
+		sliderNav.on('click', 'li', function(event){
+			event.preventDefault();
+			var selectedItem = $(this);
+			if(!selectedItem.hasClass('selected')) {
+				// if it's not already selected
+				var selectedPosition = selectedItem.index(),
+					activePosition = slidesWrapper.find('li.selected').index();
+				
+				if( activePosition < selectedPosition) {
+					nextSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, selectedPosition);
+				} else {
+					prevSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, selectedPosition);
+				}
 
-function createsoundbite(sound){
-	var html5audio=document.createElement('audio')
-	if (html5audio.canPlayType){ //check support for HTML5 audio
-		for (var i=0; i<arguments.length; i++){
-			var sourceel=document.createElement('source')
-			sourceel.setAttribute('src', arguments[i])
-			if (arguments[i].match(/\.(\w+)$/i))
-				sourceel.setAttribute('type', html5_audiotypes[RegExp.$1])
-			html5audio.appendChild(sourceel)
-		}
-		  html5audio.load()
-		  html5audio.playclip=function(){
-			html5audio.pause()
-			html5audio.currentTime=0
-			html5audio.play()
-		}
-		return html5audio
+				//this is used for the autoplay
+				visibleSlidePosition = selectedPosition;
+
+				updateSliderNavigation(sliderNav, selectedPosition);
+				updateNavigationMarker(navigationMarker, selectedPosition+1);
+				//reset autoplay
+				setAutoplay(slidesWrapper, slidesNumber, autoPlayDelay);
+			}
+		});
 	}
-	else{
-		return {playclip:function(){throw new Error("Your browser doesn't support HTML5 audio unfortunately")}}
+
+	function nextSlide(visibleSlide, container, pagination, n){
+		visibleSlide.removeClass('selected from-left from-right').addClass('is-moving').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+			visibleSlide.removeClass('is-moving');
+		});
+
+		container.children('li').eq(n).addClass('selected from-right').prevAll().addClass('move-left');
+		checkVideo(visibleSlide, container, n);
 	}
-}
 
-//Initialize two sound clips with 1 fallback file each:
+	function prevSlide(visibleSlide, container, pagination, n){
+		visibleSlide.removeClass('selected from-left from-right').addClass('is-moving').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+			visibleSlide.removeClass('is-moving');
+		});
 
-var mouseoversound=createsoundbite("http://www.cartelle.nl/media/audio/tick.ogg", "http://www.cartelle.nl/media/audio/tick.mp3")
-var clicksound=createsoundbite("http://www.ultranoir.com/cdn/gene/sound/clack.ogg", "http://www.ultranoir.com/cdn/gene/sound/clack.m4a")
+		container.children('li').eq(n).addClass('selected from-left').removeClass('move-left').nextAll().removeClass('move-left');
+		checkVideo(visibleSlide, container, n);
+	}
+
+	function updateSliderNavigation(pagination, n) {
+		var navigationDot = pagination.find('.selected');
+		navigationDot.removeClass('selected');
+		pagination.find('li').eq(n).addClass('selected');
+	}
+
+	function setAutoplay(wrapper, length, delay) {
+		if(wrapper.hasClass('autoplay')) {
+			clearInterval(autoPlayId);
+			autoPlayId = window.setInterval(function(){autoplaySlider(length)}, delay);
+		}
+	}
+
+	function autoplaySlider(length) {
+		if( visibleSlidePosition < length - 1) {
+			nextSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, visibleSlidePosition + 1);
+			visibleSlidePosition +=1;
+		} else {
+			prevSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, 0);
+			visibleSlidePosition = 0;
+		}
+		updateNavigationMarker(navigationMarker, visibleSlidePosition+1);
+		updateSliderNavigation(sliderNav, visibleSlidePosition);
+	}
+
+	function uploadVideo(container) {
+		container.find('.cd-bg-video-wrapper').each(function(){
+			var videoWrapper = $(this);
+			if( videoWrapper.is(':visible') ) {
+				// if visible - we are not on a mobile device 
+				var	videoUrl = videoWrapper.data('video'),
+					video = $('<video controls muted loop><source src="'+videoUrl+'.mp4" type="video/mp4" /><source src="'+videoUrl+'.webm" type="video/webm" /></video>');
+				video.appendTo(videoWrapper);
+				// play video if first slide
+				if(videoWrapper.parent('.cd-bg-video.selected').length > 0) video.get(0).play();
+			}
+		});
+	}
+
+	function checkVideo(hiddenSlide, container, n) {
+		//check if a video outside the viewport is playing - if yes, pause it
+		var hiddenVideo = hiddenSlide.find('video');
+		if( hiddenVideo.length > 0 ) hiddenVideo.get(0).pause();
+
+		//check if the select slide contains a video element - if yes, play the video
+		var visibleVideo = container.children('li').eq(n).find('video');
+		if( visibleVideo.length > 0 ) visibleVideo.get(0).play();
+	}
+
+	function updateNavigationMarker(marker, n) {
+		marker.removeClassPrefix('item').addClass('item-'+n);
+	}
+
+	$.fn.removeClassPrefix = function(prefix) {
+		//remove all classes starting with 'prefix'
+	    this.each(function(i, el) {
+	        var classes = el.className.split(" ").filter(function(c) {
+	            return c.lastIndexOf(prefix, 0) !== 0;
+	        });
+	        el.className = $.trim(classes.join(" "));
+	    });
+	    return this;
+	};
+});
