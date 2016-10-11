71ff4d151df58845c3e2b35d6ac52f15af55fc84
Alyssa M. Saucedo <asilla7@gmail.com>
2016-10-10 21:05:35 -0700

    update on a lot
    updated videos, fonts, responsiveness, added module, typography, links,
    svg header. new carousel

--- README.md
+++
@@ -1,2 +0,0 @@
-# googlyboobs
-googly eyes + boobs = awesomeness
--- js/main.js
+++ js/main.js
@@ -1,132 +1,63 @@
-jQuery(document).ready(function($){
-	var slidesWrapper = $('.cd-hero-slider');
+// external js: flickity.pkgd.js
 
-	//check if a .cd-hero-slider exists in the DOM 
-	if ( slidesWrapper.length > 0 ) {
-		var primaryNav = $('.cd-primary-nav'),
-			sliderNav = $('.cd-slider-nav'),
-			navigationMarker = $('.cd-marker'),
-			slidesNumber = slidesWrapper.children('li').length,
-			visibleSlidePosition = 0,
-			autoPlayId,
-			autoPlayDelay =5000;
+$(document).ready( function() {
+  var $carousel = $('.carousel');
+  var $numberofSlides = $('.carousel-cell').length;
+  $carousel.flickity({
+    contain: true,
+    wrapAround: true,
+    autoPlay: 6000,
+    accessibility: true,
+    pauseAutoPlayOnHover: false,
+    prevNextButtons: false,
+    pageDots: false,
+    initialIndex: Math.floor( Math.random() * $numberofSlides)
+  });
 
-		//upload videos (if not on mobile devices)
-		uploadVideo(slidesWrapper);
+  $("#slideshow > div:gt(0)").hide();
+  // setInterval(function() {
+  //   $('#slideshow > div:first')
+  //     .fadeOut(1000)
+  //     .next()
+  //     .fadeIn(1000)
+  //     .end()
+  //     .appendTo('#slideshow');
+  // },  6000);
 
-		//autoplay slider
-		setAutoplay(slidesWrapper, slidesNumber, autoPlayDelay);
+  function onLoadeddata( event ) {
+    var cell = $carousel.flickity( 'getParentCell', event.target );
+    $carousel.flickity( 'cellSizeChange', cell && cell.element );
+  }
 
-		//on mobile - open/close primary navigation clicking/tapping the menu icon
-		primaryNav.on('click', function(event){
-			if($(event.target).is('.cd-primary-nav')) $(this).children('ul').toggleClass('is-visible');
-		});
-		
-		//change visible slide
-		sliderNav.on('click', 'li', function(event){
-			event.preventDefault();
-			var selectedItem = $(this);
-			if(!selectedItem.hasClass('selected')) {
-				// if it's not already selected
-				var selectedPosition = selectedItem.index(),
-					activePosition = slidesWrapper.find('li.selected').index();
-				
-				if( activePosition < selectedPosition) {
-					nextSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, selectedPosition);
-				} else {
-					prevSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, selectedPosition);
-				}
+  $carousel.find('video').each( function( i, video ) {
+    video.play();
+    $(video).on( 'loadeddata', onLoadeddata );
+  });
 
-				//this is used for the autoplay
-				visibleSlidePosition = selectedPosition;
+  var $video  = $('video'),
+    $window = $(window); 
 
-				updateSliderNavigation(sliderNav, selectedPosition);
-				updateNavigationMarker(navigationMarker, selectedPosition+1);
-				//reset autoplay
-				setAutoplay(slidesWrapper, slidesNumber, autoPlayDelay);
-			}
-		});
-	}
+  $(window).resize(function(){
+      
+      var height = $window.height();
+      var width = $window.width();
+      // $video.css('height', height);
+      
+      // var videoWidth = $video.width(),
+      //     windowWidth = $window.width(),
+      // marginLeftAdjust =   (windowWidth - videoWidth);
 
-	function nextSlide(visibleSlide, container, pagination, n){
-		visibleSlide.removeClass('selected from-left from-right').addClass('is-moving').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-			visibleSlide.removeClass('is-moving');
-		});
+      // var videoWidth = $video.width(),
+      //     videoHeight = $video.height();
 
-		container.children('li').eq(n).addClass('selected from-right').prevAll().addClass('move-left');
-		checkVideo(visibleSlide, container, n);
-	}
-
-	function prevSlide(visibleSlide, container, pagination, n){
-		visibleSlide.removeClass('selected from-left from-right').addClass('is-moving').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-			visibleSlide.removeClass('is-moving');
-		});
-
-		container.children('li').eq(n).addClass('selected from-left').removeClass('move-left').nextAll().removeClass('move-left');
-		checkVideo(visibleSlide, container, n);
-	}
-
-	function updateSliderNavigation(pagination, n) {
-		var navigationDot = pagination.find('.selected');
-		navigationDot.removeClass('selected');
-		pagination.find('li').eq(n).addClass('selected');
-	}
-
-	function setAutoplay(wrapper, length, delay) {
-		if(wrapper.hasClass('autoplay')) {
-			clearInterval(autoPlayId);
-			autoPlayId = window.setInterval(function(){autoplaySlider(length)}, delay);
-		}
-	}
-
-	function autoplaySlider(length) {
-		if( visibleSlidePosition < length - 1) {
-			nextSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, visibleSlidePosition + 1);
-			visibleSlidePosition +=1;
-		} else {
-			prevSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, 0);
-			visibleSlidePosition = 0;
-		}
-		updateNavigationMarker(navigationMarker, visibleSlidePosition+1);
-		updateSliderNavigation(sliderNav, visibleSlidePosition);
-	}
-
-	function uploadVideo(container) {
-		container.find('.cd-bg-video-wrapper').each(function(){
-			var videoWrapper = $(this);
-			if( videoWrapper.is(':visible') ) {
-				// if visible - we are not on a mobile device 
-				var	videoUrl = videoWrapper.data('video'),
-					video = $('<video controls muted loop><source src="'+videoUrl+'.mp4" type="video/mp4" /><source src="'+videoUrl+'.webm" type="video/webm" /></video>');
-				video.appendTo(videoWrapper);
-				// play video if first slide
-				if(videoWrapper.parent('.cd-bg-video.selected').length > 0) video.get(0).play();
-			}
-		});
-	}
-
-	function checkVideo(hiddenSlide, container, n) {
-		//check if a video outside the viewport is playing - if yes, pause it
-		var hiddenVideo = hiddenSlide.find('video');
-		if( hiddenVideo.length > 0 ) hiddenVideo.get(0).pause();
-
-		//check if the select slide contains a video element - if yes, play the video
-		var visibleVideo = container.children('li').eq(n).find('video');
-		if( visibleVideo.length > 0 ) visibleVideo.get(0).play();
-	}
-
-	function updateNavigationMarker(marker, n) {
-		marker.removeClassPrefix('item').addClass('item-'+n);
-	}
-
-	$.fn.removeClassPrefix = function(prefix) {
-		//remove all classes starting with 'prefix'
-	    this.each(function(i, el) {
-	        var classes = el.className.split(" ").filter(function(c) {
-	            return c.lastIndexOf(prefix, 0) !== 0;
-	        });
-	        el.className = $.trim(classes.join(" "));
-	    });
-	    return this;
-	};
+      // $video.css({
+      //     'height': videoHeight, 
+      //     'width' : videoWidth
+      // });
+      
+      $video.css({
+          'height': height, 
+          'width' : width
+      });
+  }).resize();
 });
